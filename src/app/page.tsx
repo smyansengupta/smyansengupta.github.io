1503686cfbe00dc2d59b577f92160a02e07dc7cc
Smyan Sengupta <senguptasmyan@gmail.com>
2026-04-07 23:56:51 -0400

    fix formatting in about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
                 </a>
                 . I&apos;m highly driven by the applications of AI and ML to make a real-world impact.
               </p>
-              <p>
+              <p className="hero-subtitle">
                 I have comprehensive experience with Python, including NumPy, Pandas, Scikit-learn, XGBoost, Keras, and PyTorch. I have also worked extensively with Java, React/Next.js, Flutter, and SQL. I strive to keep picking up new skills every day and improving upon the ones I already have. In my free time, I enjoy traveling, plane spotting, hitting the gym, and going for bike rides. Feel free to <a href="#contact" className="text-white hover:text-gray-300 transition-colors">reach out</a> if you&apos;d like to connect or collaborate!
               </p>
             </div>
